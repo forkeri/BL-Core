225f004da1f2c2d03947e1b1ab1478e8d2360a27
forkeri <iwrbcn@qq.com>
2018-08-25 15:07:58 +0800

    Create Project

---
+++ BL-Core/CallBack/CallBack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL_Core.CallBack
+{
+    /// <summary>
+    /// 无参委托
+    /// </summary>
+    public delegate void CallBack();
+    /// <summary>
+    /// 带一个参数的委托
+    /// </summary>
+    public delegate void CallBack<T>(T obj);
+    /// <summary>
+    /// 带两个泛型参数的委托
+    /// </summary>
+    public delegate void CallBack<T1,T2>(T1 obj1,T2 obj2);
+    /// <summary>
+    /// 带三个泛型参数的委托
+    /// </summary>
+    public delegate void CallBack<T1, T2,T3>(T1 obj1, T2 obj2,T3 obj3);
+    /// <summary>
+    /// 可选参数委托
+    /// </summary>
+    public delegate void CallBacks(params object[] objs);
+}
---
+++ BL-Core/Notification/IHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL_Core.Notification
+{
+    interface IHandler
+    {
+        
+    }
+}
