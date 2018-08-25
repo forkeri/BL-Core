36af13d0f14330ec7302cce0f12477d2b93cf3a9
forkeri <iwrbcn@qq.com>
2018-08-25 17:05:13 +0800

    *

--- BL-Core/Notification/NoticeCenter.cs
+++ BL-Core/Notification/NoticeCenter.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace BL_Core.Notification
 {
-    class NoticeCenter
+    public class NoticeCenter
     {
+        private static NoticeCenter _default = new NoticeCenter();
+        public static NoticeCenter Default { get => _default; }
+
+        private Dictionary<ushort, Dictionary<IHandler, IHandler>> cmd2Handlers = null;
+        private Dictionary<IHandler, Dictionary<ushort, ushort>> handler2Cmds = null;
+
+        public NoticeCenter()
+        {
+            cmd2Handlers = new Dictionary<ushort, Dictionary<IHandler, IHandler>>();
+            handler2Cmds = new Dictionary<IHandler, Dictionary<ushort, ushort>>();
+        }
+
+        public void RegistCmd(ushort cmd, IHandler handler)
+        {
+            Dictionary<IHandler, IHandler> handlers;
+            if (cmd2Handlers.TryGetValue(cmd, out handlers))
+            {
+
+            }
+            else
+            {
+            }
+        }
     }
 }
---
+++ BL-Core/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL_Core
+{
+    /// <summary>
+    /// 单例模板，满足条件：类，无参构造器
+    /// </summary>
+    public class Singleton<T> where T : class, new()
+    {
+        public static T _instance = null;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new T();
+                return _instance;
+            }
+        }
+    }
+}
